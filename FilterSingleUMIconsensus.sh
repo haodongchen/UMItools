8b143f79879de52c0ce4c8f30ce98b0fbd12731f
Haodong Chen <chen.hdong@gmail.com>
2020-01-24 14:59:24 -0800

    Update FilterSingleUMIconsensus.sh
    Add timestamps and progress to the log.

--- FilterSingleUMIconsensus.sh
+++ FilterSingleUMIconsensus.sh
@@ -14,13 +14,18 @@ samtools='/path/to/samtools-1.9/samtools'
 sentieon='/path/to/sentieon-genomics-201911/bin/sentieon'
 
 $samtools view -@ 3 -h $input_bam | \
-awk -v MinR=$min_read -v OFS="\t" ' 
+awk -v MinR=$min_read -v OFS="\t" '
+BEGIN {reads=0; reads2=0; print "["strftime("Time = %m/%d/%Y %H:%M:%S", systime())"]  Executing FilterSingleUMIconsensus."> "/dev/stderr"}
 { if ($0~/^@/) {print;} 
   else {
-      for(i=12;i<=NF;i++){ if($i~/^XZ:Z:/) split($i,a,":") }
-      if ( a[3]>=MinR ) print;
-       }
-}' | \
+      for(i=NF;i>=12;i--){ if($i~/^XZ:Z:/) {split($i,a,":");break;} }
+      if ( a[3]>=MinR ) {reads2++;print;}
+      reads++;
+      if (reads % 2000000 == 0) {
+          print "["strftime("Time = %m/%d/%Y %H:%M:%S", systime())"]  Processed "reads" reads.  Last read position: "$3":"$4> "/dev/stderr"}
+      }
+}
+END {print "["strftime("Time = %m/%d/%Y %H:%M:%S", systime())"]  Done. Processed "reads" reads.  "reads2" reads passed the filter."> "/dev/stderr" } ' | \
 $samtools view -bh - > $output_bam
 
 $sentieon util index $output_bam
