ddb60a2e0b3fd2ca54e5d73e96a9f2841278b022
Haodong Chen <chen.hdong@gmail.com>
2020-01-27 12:45:50 -0800

    Update FilterDuplexUMIconsensus.sh
    Add timestamps and progress to the log

--- FilterDuplexUMIconsensus.sh
+++ FilterDuplexUMIconsensus.sh
@@ -29,13 +29,23 @@ function max(b) {
 function sum(b) {
    return b[1]+b[2]
 }
-BEGIN {split(MinR,tmp,","); mr1=tmp[1]; mr2=tmp[2]<tmp[3]?tmp[2]:tmp[3]; mr3=tmp[2]<tmp[3]?tmp[3]:tmp[2]}
+BEGIN {split(MinR,tmp,","); 
+       mr1=tmp[1]; 
+       mr2=tmp[2]<tmp[3]?tmp[2]:tmp[3]; 
+       mr3=tmp[2]<tmp[3]?tmp[3]:tmp[2];
+       reads=0; reads2=0; 
+       print "["strftime("Time = %m/%d/%Y %H:%M:%S", systime())"]  Executing FilterDuplexUMIconsensus."> "/dev/stderr"
+}
 { if ($0~/^@/) {print;} 
   else {
-      for(i=12;i<=NF;i++){ if($i~/^XZ:Z:/){split($i,a,":");split(a[3],b,",")}}
-      if (sum(b)>=mr1 && min(b)>=mr2 && max(b)>=mr3 ) print;
+      for(i=NF;i>=12;i--){ if($i~/^XZ:Z:/) {split($i,a,":");split(a[3],b,","); break;}}
+      if (sum(b)>=mr1 && min(b)>=mr2 && max(b)>=mr3 ) {reads2++;print;}
+      reads++;
+      if (reads % 2000000 == 0) {
+          print "["strftime("Time = %m/%d/%Y %H:%M:%S", systime())"]  Processed "reads" reads.  Last read position: "$3":"$4> "/dev/stderr"}
        }
-}' | \
+}
+END {print "["strftime("Time = %m/%d/%Y %H:%M:%S", systime())"]  Done. Processed "reads" reads.  "reads2" reads passed the filter."> "/dev/stderr" } ' | \
 $samtools view -bh - > $output_bam
 
 $sentieon util index $output_bam
